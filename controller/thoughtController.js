0c3bd8f4170051e1dd7af45eb67a00750f9152b0
DanielK <Danielkang13@gmail.com>
2022-09-19 16:18:51 -0700

    added get function for grabbing one thought, added post and put function, added routes

--- controller/thoughtController.js
+++ controller/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require('../model')
+const { Thought, User } = require('../model')
 
 module.exports = {
     getAllThoughts(req,res) {
@@ -8,6 +8,49 @@ module.exports = {
             } else {
                 res.status(200).json(thoughts);
             }
-        })
+        });
     },
+    getOneThought(req,res) {
+        Thought.findOne({_id: req.params.thoughtId}, function(err, thought) {
+            if (err) {
+                res.status(500).json({message: "Find one thought Error"});
+            } else {
+                res.status(200).json(thought);
+            }
+        }); 
+    },
+    createThought(req,res) {
+        Thought.create({ thoughtText: req.body.thoughtText, username: req.body.username,},
+            function(err, thought) {
+                if (err) {
+                    res.status(500).json({message: "Creating Thought Error"});
+                } else {
+                    User.findOneAndUpdate(
+                        {_id: req.body.userId},
+                        {$addToSet: { thoughts: thought._id }},
+                        {new: true},
+                        function(err, user) {
+                            if (err) {
+                                res.status(500).json({message: "Adding thought to user error!"});
+                            } else {
+                                res.status(200).json(user);
+                            }
+                    });
+                }
+            });
+    },
+    updateThought(req,res) {
+        Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            { thoughtText: req.body.thoughtText },
+            {new: true},
+            function(err, thought) {
+                if (err) {
+                    res.status(500).json({message: "Update thought Error"});
+                } else {
+                    res.status(200).json(thought);
+                }
+            }
+        );
+    }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 //grab the route action functions from controller
 const { 
     getAllThoughts,
+    getOneThought,
+    createThought,
+    updateThought,
 } = require('../../controller/thoughtController');
 
-router.route('/').get(getAllThoughts);
+router.route('/').get(getAllThoughts).post(createThought)
+
+router.route('/:thoughtId').get(getOneThought).put(updateThought)
 
 module.exports = router;
